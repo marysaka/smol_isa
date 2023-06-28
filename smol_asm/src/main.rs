70ecee84a3f123fedea9cef83de72ca99ba60cef
Miika Alikirri <nykseli@protonmail.com>
2023-06-28 21:15:56 +0300

    Compiler asm file into object

--- smol_asm/src/main.rs
+++ smol_asm/src/main.rs
@@ -1,3 +1,5 @@
+use std::{fmt::format, fs, process::exit};
+
 mod ast;
 mod compiler;
 
@@ -8,7 +10,14 @@ const FILE_DATA: &str = "
 ";
 
 fn main() {
-    let tree = ast::parse_source(FILE_DATA).unwrap();
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Give file as an argument");
+        exit(1);
+    }
+
+    let file_contents = fs::read_to_string(&args[1]).unwrap();
+    let tree = ast::parse_source(&file_contents).unwrap();
     let binary = compiler::compile_instructions(tree);
-    println!("{binary:#?}");
+    fs::write(format!("{}.obj", &args[1]), binary).unwrap();
 }
